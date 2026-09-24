97a6c25ba191fa10fc44b3dfe4d85c70a78ef3c3
mishga <minshanmak1@gmail.com>
2026-09-24 10:25:07 +0530

    fix(assets): restore missing logo and favicon lost during purge

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,6 +30,7 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${dmSans.variable} ${playfair.variable}`}>
       <head>
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
       </head>
       <body>
